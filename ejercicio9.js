ced900ae9304708bd78a35830d6d988dac24a20e
GersonBenito <gersonbenito4@gmail.com>
2023-05-01 16:44:23 -0600

    fix: ejercicio 9

--- ejercicio9.js
+++ ejercicio9.js
@@ -5,20 +5,11 @@
 
 const elementosComunes = (array1, array2) => {
     let numeros = [];
-    let numerosComunes = [];
 
-    for(let i of array1){
-        for(j of array2){
-            if(i === j){
-                numeros.push(i);
-            }
-        }
-    }
+    numeros = array1.filter(item => array2.includes(item));
+    numeros = numeros.sort((a, b) => a - b); // ordenar los numeros de menor a mayor
 
-    numerosComunes = numeros.filter((element,indice, array ) => array.indexOf(element) === indice); // sacar los numeros repetidos
-    numerosComunes = numerosComunes.sort((a, b) => a - b); // ordenar los numeros de menor a mayor
-    
-    return numerosComunes;
+    return numeros;
 }
 
 const result = elementosComunes([4,,8,5,6,7], [7,8,9,7,5]);
